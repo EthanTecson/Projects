5a7262c7d6c81af7d5797a47575a81abe8963259
Ethan Tecson <105937871+EthanTecson@users.noreply.github.com>
2022-11-22 11:33:55 -0500

    Update AutoAffordabilityCalculator.py

--- AutoAffordabilityCalculator.py
+++ AutoAffordabilityCalculator.py
@@ -11,7 +11,17 @@ Created on Sun Nov 20 14:12:34 2022
 #######################################################################
 def average_salary(total, tax, down_payment, term, maintenance, insurace):
     """
+    This function will take the total cost of an automobile and run it 
+    through calculations including tax, down payment, payment term, maintenance,
+    and insurance to produce an average salary needed to COMFORTABLY afford the
+    chosen autombile based off of the 10% of income rule. 
           Parameters:
+               total -- initial total cost of automobile
+               tax -- tax rate based off state expressed as percentage
+               down_payment -- desired down payment expressed as percentage
+               term -- desired loan term 
+               maintenance -- average maintenance cost of automobile 
+               insurace -- average insurance cost of automobile 
           Returns:
     """
     print(total)
@@ -33,7 +43,10 @@ def average_salary(total, tax, down_payment, term, maintenance, insurace):
 ######################################################################
 def main():
      """
-     This main function...
+     This main function will prompt the user to input the total cost of the car,
+     tax rate in state (becasue this varies based off of state), down payment in percentage,
+     desired loan term, average mintenance cost, and average insurance cost. The function
+     will then take all inputs and use them as the parameters of the average_salary() function. 
      """
      auto_cost = int(input('Total Cost of Car: '))
      tax_rate = int(input('Tax Rate in State (In Percentage): %'))
